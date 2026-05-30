664808c32befb6a992419ba14f4d1ee89703bbb0
Sawille <71983016+Sawille@users.noreply.github.com>
2026-05-30 08:04:32 +0200

    Update TicTacToe to export calculateWinner for testing

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -58,7 +58,8 @@ export default function Game() {
   );
 }
 
-function calculateWinner(squares) {
+// Export for testing
+export function calculateWinner(squares) {
   const lines = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
